f0a3d1fcf7ccafb369f6ede6003beadbf68b36f9
Jinho Chun <jukoku@gmail.com>
2023-04-25 12:34:52 +0900

    섹션 15 - 73. REST API:Add New Custom Route(URL)

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -56,6 +56,7 @@ register_post_type('campus', array(
 
    // Professor Post Type
    register_post_type('professor', array(
+    'show_in_rest' => true,
     'supports' => array('title', 'editor', 'thumbnail'),    
     'public' => true,
     'show_in_rest' => true,
