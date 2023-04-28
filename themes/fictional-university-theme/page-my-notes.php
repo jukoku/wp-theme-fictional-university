d1acf9f12f47e02ef7a769456a87b27efd6d6cee
Jinho Chun <jukoku@gmail.com>
2023-04-28 19:41:54 +0900

    퇴근으로 일단 저장

--- themes/fictional-university-theme/page-my-notes.php
+++ themes/fictional-university-theme/page-my-notes.php
@@ -12,7 +12,13 @@
     pageBanner();
     ?>
     
-    <div class="container container--narrow page-section">     
+    <div class="container container--narrow page-section">
+      <div class="create-note">
+        <h2 class="headline headline--medium">Create New Note</h2>
+        <input class="new-note-title" placeholder="title">
+        <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+        <span class="submit-note">Create Note</span>
+      </div>
       <ul clas="min-list link-list" id="my-notes">
         <?php
           $userNotes = new WP_Query(array(
